3b16383bdea4318bbdf4bc93f2110a8e83f85841
Raffael Wagner <raffaelwagner@gmail.com>
2023-05-17 18:47:15 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: '1',
+    photoCover:
+      'https://hermes.dio.me/tracks/a123a707-54de-4a67-88ee-5c129beb14d7.png',
+    title: 'Potência Tech Angular Developer',
+    description:
+      'O Potência Tech powered by iFood voltou com tudo e agora traz um Bootcamp focado em Angular! Com cursos, desafios de projeto, desafios de código e mentorias exclusivas, o Bootcamp Potência Tech Angular Developer - Powered by iFood traz uma experiência completa para você que quer dar um pontapé na carreira tech! As inscrições já estão abertas para pessoas de todo o Brasil, mas voltado e focado em grupos sub-representados e pessoas de baixa renda. Ao final do programa, acontecerá o Minha Carreira Tech, uma oportunidade de conhecer as empresas parceiras e ficar disponível na plataforma de talentos da DIO, além da possibilidade de conquistar prêmios pelo seu desempenho.',
+  },
+  {
+    id: '2',
+    photoCover:
+      'https://hermes.digitalinnovation.one/certificates/cover/E38C1377.jpg',
+    title: 'Módulos Angular',
+    description:
+      'O aluno Raffael Wagner, cadastrado na Potência Tech Angular Developer - Powered by iFood, terminou no dia 10 de Maio de 2023 mais um curso presente em um dos módulos do Bootcamp.',
+  },
+  {
+    id: '3',
+    photoCover:
+      'https://hermes.digitalinnovation.one/certificates/cover/B2CC50C3.jpg',
+    title: 'Single Page Application com Angular',
+    description:
+      'O aluno Raffael Wagner, cadastrado na Potência Tech Angular Developer - Powered by iFood, terminou no dia 10 de Maio de 2023 mais um curso presente em um dos módulos do Bootcamp.',
+  },
+  {
+    id: '4',
+    photoCover:
+      'https://hermes.digitalinnovation.one/certificates/cover/D6BB1702.jpg',
+    title: 'LifeCycle Hooks Angular',
+    description:
+      'O aluno Raffael Wagner, cadastrado na Potência Tech Angular Developer - Powered by iFood, terminou no dia 06 de Maio de 2023 mais um curso presente em um dos módulos do Bootcamp.',
+  },
+];
